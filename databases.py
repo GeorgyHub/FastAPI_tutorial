b4a5af2b37d85b28febf133add837c1b721b5b58
GeorgyHub <georgy.polkanov@yandex.ru>
2024-10-30 20:12:09 +0300

    Edit database

--- databases.py
+++ databases.py
@@ -1,13 +1,13 @@
 from typing import Optional
 
-from sqlalchemy.ext.asynco import create_async_engine, asynco_sessionmaker, engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-create_async_engine(
+engine = create_async_engine(
     "sqlite+aiosqlite:///tasks.db"
 )
 
-new_session = asynco_sessionmaker(engine, expire_on_commit=True)
+new_session = async_sessionmaker(engine, expire_on_commit=True)
 
 class Model(DeclarativeBase):
     pass
--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from typing import Optional, Annotated
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import Depends
+from contextlib import asynccontextmanager
+
+# import files
+from databases import create_tables, delete_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База готова")
+    yield
+    print("Выключение")
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 app = FastAPI()
 
